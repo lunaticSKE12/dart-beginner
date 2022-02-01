b949233620ffab8b9488cb8cb0dccb48c2dba925
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-02-01 20:53:26 +0700

    Try/catch and JSON

--- bin/http.dart
+++ bin/http.dart
@@ -1,15 +1,23 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   Future<void> getData() async {
-    Uri url = Uri.parse(
-        'http://api.exchangeratesapi.io/v1/latest?access_key=892e88ea20621e1235f09c4a46adc578&format=1');
-    Response result = await get(url);
-    print(result.body);
+    try {
+      Uri url = Uri.parse(
+          'http://api.exchangeratesapi.io/v1/latest?access_key=892e88ea20621e1235f09c4a46adc578&format=1');
+      http.Response result = await http.get(url);
+      var body = jsonDecode(result.body);
+      print(body['base']);
+      double thb = body['rates']['THB'];
+      print(thb);
 
-    // old
-    // Response result = await get('');
-    // print(result.body);
+      // old
+      // Response result = await get('');
+      // print(result.body);
+    } catch (error) {
+      print(error);
+    }
   }
 
   getData();
